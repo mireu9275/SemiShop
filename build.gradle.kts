9e72db695786980fc5266e55023dc4af331817cd
Hojung Kim <tora4094@inha.edu>
2024-07-05 22:28:04 +0900

    feature: paper 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.pm20.util.archivesName
+
 plugins {
     kotlin("jvm") version "1.9.23"
 }
@@ -7,9 +9,12 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.20.2-R0.1-SNAPSHOT")
+    compileOnly("kr.eme.semiMoney:SemiMoney:1.0-SNAPSHOT")
     testImplementation(kotlin("test"))
 }
 
@@ -18,4 +23,12 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(17)
+}
+
+tasks.jar {
+    archivesName = "${project.name}-${project.version}.jar"
+    destinationDirectory = file("d:\\minecraft\\1. 버킷 관련\\1.20.2 paper_dev2\\plugins")
+    manifest {
+        attributes["main-class"] = "kr.eme.semiShop.SemiShop"
+    }
 }
